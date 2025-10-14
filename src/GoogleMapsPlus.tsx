d1ae90aa2cf37aca7efcf5db1fad184e2cb75e9c
pinpong <o.nitzschke@gmail.com>
2025-10-14 11:44:31 +0700

    build: remove commonjs

--- src/GoogleMapsPlus.tsx
+++ src/GoogleMapsPlus.tsx
@@ -1,13 +1,13 @@
 import { getHostComponent, NitroModules } from 'react-native-nitro-modules';
 
-import ViewConfig from '../nitrogen/generated/shared/json/RNGoogleMapsPlusViewConfig.json';
+import ViewConfig from '../nitrogen/generated/shared/json/RNGoogleMapsPlusViewConfig.json' with { type: 'json' };
 
 import type {
   RNGoogleMapsPlusViewMethods,
   RNGoogleMapsPlusViewProps,
-} from './RNGoogleMapsPlusView.nitro';
+} from './RNGoogleMapsPlusView.nitro.js';
 
-import type { RNGoogleMapsPlusModule } from './RNGoogleMapsPlusModule.nitro';
+import type { RNGoogleMapsPlusModule } from './RNGoogleMapsPlusModule.nitro.js';
 
 export const GoogleMapsView = getHostComponent<
   RNGoogleMapsPlusViewProps,
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import { GoogleMapsView, GoogleMapsModule } from './GoogleMapsPlus';
+import { GoogleMapsView, GoogleMapsModule } from './GoogleMapsPlus.js';
 import type {
   RNGoogleMapsPlusViewMethods,
   RNGoogleMapsPlusViewProps,
 } from './RNGoogleMapsPlusView.nitro';
 import type { RNGoogleMapsPlusModule } from './RNGoogleMapsPlusModule.nitro';
 
-export * from './types';
+export * from './types.js';
 
 export type {
   RNGoogleMapsPlusViewMethods,
