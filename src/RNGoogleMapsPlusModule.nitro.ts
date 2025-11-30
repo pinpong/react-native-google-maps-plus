5a3e4861861c88cf2c920ae85a953b6360460c2d
pinpong <o.nitzschke@gmail.com>
2025-11-30 18:38:10 +0700

    chore: update dependencies

--- src/RNGoogleMapsPlusModule.nitro.ts
+++ src/RNGoogleMapsPlusModule.nitro.ts
@@ -2,8 +2,10 @@ import { type HybridObject } from 'react-native-nitro-modules';
 
 import type { RNLocationPermissionResult } from './types';
 
-export interface RNGoogleMapsPlusModule
-  extends HybridObject<{ ios: 'swift'; android: 'kotlin' }> {
+export interface RNGoogleMapsPlusModule extends HybridObject<{
+  ios: 'swift';
+  android: 'kotlin';
+}> {
   showLocationDialog(): void;
 
   openLocationSettings(): void;
