1427a4eab2ce403326703ce04eebefe67f815ce6
pinpong <o.nitzschke@gmail.com>
2026-03-05 21:26:33 +0700

    fix: update to nitro v0.35.0

--- android/src/main/cpp/cpp-adapter.cpp
+++ android/src/main/cpp/cpp-adapter.cpp
@@ -1,6 +1,9 @@
+#include <fbjni/fbjni.h>
 #include <jni.h>
 #include "RNGoogleMapsPlusOnLoad.hpp"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::rngooglemapsplus::initialize(vm);
+  return facebook::jni::initialize(vm, []() {
+    margelo::nitro::rngooglemapsplus::registerAllNatives();
+  });
 }
